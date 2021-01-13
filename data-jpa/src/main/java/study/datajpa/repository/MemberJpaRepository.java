d7b3fa2d02c0e08b93d9bb5186fede3c0fba2c2e
hyun6ik <fkanwm@naver.com>
2021-01-14 05:28:27 +0900

    스프링 데이터 JPA와 DB설정, 동작확인

---
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member){
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m ", Member.class)
+                .getResultList();
+    }
+
+}
