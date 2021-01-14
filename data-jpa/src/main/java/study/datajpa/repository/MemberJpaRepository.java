48113517f9a0d87233e24d989c611e9f5155faa2
hyun6ik <fkanwm@naver.com>
2021-01-14 21:31:09 +0900

    순수 JPA기반 Repository 만들기

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -7,6 +7,7 @@ import study.datajpa.entity.Member;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -19,8 +20,22 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member){
+        em.remove(member);
+    }
+
     public Member find(Long id){
         return em.find(Member.class, id);
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public List<Member> findAll(){
