a171d677d7cb82275f5d88ece49f55ce7318ee39
hyun6ik <fkanwm@naver.com>
2021-01-15 23:35:56 +0900

    @Query, Repository 메소드에 쿼리 정의하기

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -108,4 +108,18 @@ public class MemberRepositoryTest {
         //then
         assertThat(findMember).isEqualTo(m1);
     }
+
+    @Test
+    public void testQuery() throws Exception {
+        //given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        //when
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        //then
+        assertThat(result).isEqualTo(m1);
+    }
 }
