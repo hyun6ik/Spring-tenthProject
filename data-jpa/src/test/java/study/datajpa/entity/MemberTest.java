2f3221de294d38b3447c688fd6cb53f305db5ca3
hyun6ik <fkanwm@naver.com>
2021-01-17 21:18:50 +0900

    Auditing @EntityListeners(AuditingEntityListener.class) @MappedSuperclass
    생성시간 :   @CreatedDate
               @Column(updatable = false)
    
    수정시간 :   @LastModifiedDate

--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,9 +1,11 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,8 +19,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Rollback(false)
 class MemberTest {
     
-    @PersistenceContext
-    private EntityManager em;
+    @PersistenceContext private EntityManager em;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testEntity()  throws Exception {
@@ -50,5 +52,27 @@ class MemberTest {
         }
         //then
         
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member);  // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush();  //@PreUpdate
+        em.clear();
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+        //then
+        System.out.println("findMember.getCreatedDate() = " + findMember.getCreatedDate());
+        System.out.println("findMember.getLastModifiedDate() = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreatedBy() = " + findMember.getCreatedBy());
+        System.out.println("findMember.getLastModifiedBy() = " + findMember.getLastModifiedBy());
+
+
     }
 }
